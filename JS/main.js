fe4cee0d34b5f07a740165dfdacd9e3ff0231fb1
3MAR-ELBORA3EY <98759405+3MAR-ELBORA3EY@users.noreply.github.com>
2024-07-22 17:25:21 +0300

    Add files via upload

--- JS/main.js
+++ JS/main.js
@@ -16,7 +16,7 @@ function header() {
   toggleScreen();
   function toggleScreen() {
     let nav = document.querySelector("header nav");
-    if (window.innerWidth < 750) {
+    if (window.innerWidth < 1000) {
       nav.classList.remove("desktop");
       nav.classList.add("mobile");
     } else {
@@ -42,4 +42,48 @@ function header() {
       });
     }
   }
+}
+
+if (window.innerWidth < 750) {
+  FAQS();
+}
+let currentWidth = window.innerWidth;
+
+window.addEventListener("resize", () => {
+  if (window.innerWidth != currentWidth) {
+    if (window.innerWidth < 750) {
+      FAQS();
+      currentWidth = window.innerWidth;
+    } else {
+      let questions = document.querySelectorAll(".faqs .question");
+      questions.forEach((e) => {
+        e.style.height = "fit-content";
+      });
+    }
+  }
+});
+function FAQS() {
+  let questions = document.querySelectorAll(".faqs .question");
+  questions.forEach((e) => {
+    let ask = e.querySelector(".ask");
+    let askHeight = ask.offsetHeight;
+    let answer = e.querySelector(".answer");
+    let answerHeight = answer.scrollHeight;
+    e.style.height = `${askHeight - 1}px`;
+    ask.onclick = () => {
+      if (e.classList.contains("drobed")) {
+        e.style.height = `${askHeight - 1}px`;
+        e.classList.remove("drobed");
+      } else {
+        questions.forEach((q) => {
+          let ask = q.querySelector(".ask");
+          let askHeight = ask.offsetHeight;
+          q.style.height = `${askHeight - 1}px`;
+          q.classList.remove("drobed");
+        });
+        e.style.height = `${answerHeight + askHeight}px`;
+        e.classList.add("drobed");
+      }
+    };
+  });
 }
